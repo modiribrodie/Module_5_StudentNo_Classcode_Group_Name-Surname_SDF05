778cd81b5513e2d5c6b7e1796de18cc72f90689a
modiribrodie <modiribrodi@gmail.com>
2024-04-23 22:57:25 +0200

    A Black Jack game
    *Blackjack is a card game played with a standard deck of 52 cards.
    *The goal of the game is to have a hand with a total value of 21 or as close to 21 as possible without going over.
    *The value of the cards is as follows: numbered cards (2-10) are worth their face value, face cards (Jack, Queen, King) are worth 10, and Aces can be worth either 1 or 11.
    *The game starts with the player and the dealer each receiving two cards.
    *The player can then choose to "hit" (receive another card) or "stand" (keep their current hand).
    *If the player's hand exceeds 21, they "bust" and lose the game.
    *If the player's hand is closer to 21 than the dealer's hand, the player wins.

--- index.js
+++ index.js
@@ -1,11 +1,63 @@
-// 1. Create two variables, firstCard and secondCard. 
-// Set their values to a random number between 2-11
+let player = {
+    name: "Per",
+    chips: 200
+}
 
-// 2. Create a variable, sum, and set it to the sum of the two cards
+let cards = []
+let sum = 0
+let hasBlackjack = false
+let isAlive = false
+let message = ""
+let messageContent = document.getElementById("message-content")
+let sumContent = document.getElementById("sum-content")
+let cardsContent = document.getElementById("cards-content")
 
-let firstCard = 6
-let secondCard = 9
+playerContent.textContent = player.name + ": $" + player.chips
 
-let sum = firstCard + secondCard
+function getRandomCard() {
+    let num = Math.floor(Math.random() *13 ) + 1
+    if (num === 1) {
+        return 11
+    } else if (num === 11 || num === 12 || num === 13) {
+        return 10
+    } else {
+        return num
+    }
+}
 
-console.log(sum)
+function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+    renderGame()
+}
+
+function renderGame() {
+    cardsContent.textContent = "Cards: "
+    for (let i = 0; i < cards.length; i++) {
+        cardsContent.textContent += " " + cards[i]
+    }
+    
+    sumContent.textContent = "Sum: " + sum
+    if (sum < 21) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackjack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    messageContent.textContent = message
+}
+
+function newCard() {
+    if (isAlive) {
+        let card = getRandomCard()
+        cards.push(card)
+        sum += card
+        renderGame()        
+    }
+}
